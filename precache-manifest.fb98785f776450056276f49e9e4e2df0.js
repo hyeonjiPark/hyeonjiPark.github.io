cd63f58b88fff1ab5a91b7cf6323eaf217670cf5
박현지 <phjgina88@gmail.com>
2020-02-19 20:08:38 +0900

    first commit

---
+++ precache-manifest.fb98785f776450056276f49e9e4e2df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa389b5fc011cda68519173509feb2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26711d1d5ce8633b59ad",
+    "url": "/static/css/main.bb2754ff.chunk.css"
+  },
+  {
+    "revision": "0408ce7a0ff4c9e50fbd",
+    "url": "/static/js/2.ea5d773a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ea5d773a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26711d1d5ce8633b59ad",
+    "url": "/static/js/main.103bf5db.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  }
+]);
